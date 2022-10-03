df393b3f1f197f22a3f2047bc7f3b1ed41b4e19a
Bozberkeoz <email>
2022-10-03 14:14:34 +0000

    col3 added

--- dbo/Tables/Creation-Scripts/Table_1.sql
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -4,7 +4,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_1](
 	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[col3] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
 GO
